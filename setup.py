c708b69a6d9de77131f14f47ec9f2cc0da45a8a8
Nishant Shelar <nishant8887@gmail.com>
2015-12-25 21:17:30 +0530

    Some more changes to setup.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Nishant Shelar',
     author_email='nishant8887@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=open('%saproposdrive/requirements.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
+    install_requires=open('%s/wsgi/requirements.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
     dependency_links=[
         'https://pypi.python.org/simple/django/'
     ],
